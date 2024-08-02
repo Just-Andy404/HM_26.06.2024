06675c67a8f4a1d5917e7de58ba32d8384fa178d
Andy <andyivanov2007@gmail.com>
2024-08-02 22:44:09 +0300

    Add project files.

---
+++ ConsoleApp1/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public struct IpData
+{
+    public string Country { get; set; }
+    public string Region { get; set; }
+    public string City { get; set; }
+
+    public IpData(string country, string region, string city)
+    {
+        Country = country;
+        Region = region;
+        City = city;
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿using hw_cSH_14;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const string path = "data.json";
+
+            Query.GetLocationByIp("62.16.8.251", path);
+            //Query.GetLocationByIp("8.8.4.4", path);
+            //Query.GetLocationByIp("32.16.8.221", path);
+
+            Console.ReadKey();
+
+        }
+    }
+}
